7adb4f0332492d6f7e77191395be41e40c2238a2
vitorsam021 <vitorlimasilva04@gmail.com>
2026-02-20 00:13:13 -0300

    Criando User

---
+++ src/main/java/com/smk/apiproject/models/User.java
@@ -0,0 +1,34 @@
+package com.smk.apiproject.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = User.USER_NAME)
+public class User {
+
+    public static final String USER_NAME = "user";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "password", length = 100, nullable = false, unique = true)
+    @NotNull
+    @NotEmpty
+    @Size(min = 3,max = 100)
+    private String username;
+    
+    @Column(name = "password", length = 60, nullable = false, unique = true)
+    @NotNull
+    @NotEmpty
+    @Size(min = 8,max = 60)
+    private String password;
+
+
+
+}
